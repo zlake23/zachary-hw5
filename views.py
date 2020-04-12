c3059509624c02ce474ff4313754d5f6f592aee3
Zachary Lake <zlake23@gmail.com>
2020-04-12 12:54:42 -0700

    Updated view functions for django templating

--- views.py
+++ views.py
@@ -2,38 +2,26 @@ import requests
 from django.shortcuts import render
 
 def home(request):
-    about_html = open("content/about.html").read()
     context = {
-        "content": about_html,
-        "title": "Home Page",
         "active_home": "active",
     }
-    return render(request, "base.html", context)
+    return render(request, "about.html", context)
 
 
 def blog(request):
-    blog_html = open("content/blog.html").read()
     context = {
-        "content": blog_html,
-        "title": "My Blog",
         "active_blog": "active",
     }
-    return render(request, "base.html", context)
+    return render(request, "blog.html", context)
 
 def contact(request):
-    contact_html = open("content/contact.html").read()
     context = {
-        "content": contact_html,
-        "title": "Contact Page",
         "active_contact": "active",
     }
-    return render(request, "base.html", context)
+    return render(request, "contact.html", context)
 
 def projects(request):
-    projects_html = open("content/projects.html").read()
     context = {
-        "content": projects_html,
-        "title": "My Projects",
         "active_projects": "active",
     }
-    return render(request, "base.html", context)
+    return render(request, "projects.html", context)
