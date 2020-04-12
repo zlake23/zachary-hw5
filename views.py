d81f5cff7f76345da8c88d925b758c4b582350b6
Zachary Lake <zlake23@gmail.com>
2020-04-12 10:14:50 -0700

    Added title key in context dictionary

--- views.py
+++ views.py
@@ -5,6 +5,7 @@ def home(request):
     about_html = open("content/about.html").read()
     context = {
         "content": about_html,
+        "title": "Home Page",
     }
     return render(request, "base.html", context)
 
@@ -12,20 +13,23 @@ def home(request):
 def blog(request):
     blog_html = open("content/blog.html").read()
     context = {
-        'content': blog_html,
+        "content": blog_html,
+        "title": "My Blog",
     }
     return render(request, "base.html", context)
 
 def contact(request):
     contact_html = open("content/contact.html").read()
     context = {
-        'content': contact_html,
+        "content": contact_html,
+        "title": "Contact Page",
     }
     return render(request, "base.html", context)
 
 def projects(request):
     projects_html = open("content/projects.html").read()
     context = {
-        'content': projects_html,
+        "content": projects_html,
+        "title": "My Projects",
     }
     return render(request, "base.html", context)
